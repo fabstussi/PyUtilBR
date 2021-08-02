d08221dd80fb0d748decb050bfcaa56da301a007
Fabiano Stussi Pereria <66576716+fabstussi@users.noreply.github.com>
2021-08-02 17:39:36 -0300

    Retirada a classe
    Classes desnecessárias

--- PyUtilTerminal.py
+++ PyUtilTerminal.py
@@ -7,43 +7,34 @@ import os
 import platform
 
 
-class PyUtilTerm:
+def limpa_tela():
+    os.system('cls' if platform.system() == 'Windows' else 'clear')
 
-    @staticmethod
-    def limpa_tela():
-        os.system('cls' if platform.system() == 'Windows' else 'clear')
 
-    @staticmethod
-    def desenha_linha(simbolo: str, tamanho=10):
-        print(simbolo * tamanho)
+def desenha_linha(simbolo: str, tamanho=10):
+    print(simbolo * tamanho)
 
-    @staticmethod
-    def titulo(texto: str, simbulo='*'):
-        texto = f'** {texto} **'
-        PyUtilTerm.desenha_linha(f'{simbulo}', len(texto))
-        print(texto)
-        PyUtilTerm.desenha_linha(f'{simbulo}', len(texto))
 
-    @staticmethod
-    def titulo_ml(titulos: list):
-        maior_texto = len(titulos[0])
-        for texto in titulos:
-            tamanho_texto = len(texto) if len(texto) % 2 == 0 else len(texto) + 1
-            if tamanho_texto > maior_texto:
-                maior_texto = tamanho_texto
-        PyUtilTerm.desenha_linha('=', maior_texto + 4)
-        for texto in titulos:
-            centraliza = (maior_texto - len(texto)) // 2
-            print(f'|{" " * centraliza} {texto} '
-                  f'{" " * centraliza if len(texto) % 2 == 0 else " " * (centraliza + 1)}|')
-        PyUtilTerm.desenha_linha('=', maior_texto + 4)
+def titulo(texto: str, simbulo='*'):
+    texto = f'** {texto} **'
+    desenha_linha(f'{simbulo}', len(texto))
+    print(texto)
+    desenha_linha(f'{simbulo}', len(texto))
+
+
+def titulo_ml(titulos: list):
+    maior_texto = len(titulos[0])
+    for texto in titulos:
+        tamanho_texto = len(texto) if len(texto) % 2 == 0 else len(texto) + 1
+        if tamanho_texto > maior_texto:
+            maior_texto = tamanho_texto
+    desenha_linha('=', maior_texto + 4)
+    for texto in titulos:
+        centraliza = (maior_texto - len(texto)) // 2
+        print(f'|{" " * centraliza} {texto} '
+              f'{" " * centraliza if len(texto) % 2 == 0 else " " * (centraliza + 1)}|')
+    desenha_linha('=', maior_texto + 4)
 
 
 if __name__ == '__main__':
-    PyUtilTerm.limpa_tela()
-    aviso = [
-        'Desenvolvido por',
-        'Fabiano Stussi Pereira',
-        'Em 01/08/2021'
-    ]
-    PyUtilTerm.titulo_ml(aviso)
+    pass
