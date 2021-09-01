4dfdabb20a2ced35fe664b24b0255b817c4f2cf0
Fabiano Stussi Pereria <66576716+fabstussi@users.noreply.github.com>
2021-08-31 21:32:06 -0300

    Adicionado cria_menu
    Deixei o código conforme a PeP8 e foi adicionada a função cria_menu()

--- PyUtilTerminal.py
+++ PyUtilTerminal.py
@@ -25,15 +25,33 @@ def titulo(texto: str, simbulo='*'):
 def titulo_ml(titulos: list):
     maior_texto = len(titulos[0])
     for texto in titulos:
-        tamanho_texto = len(texto) if len(texto) % 2 == 0 else len(texto) + 1
+        tamanho_texto = len(texto)
         if tamanho_texto > maior_texto:
             maior_texto = tamanho_texto
     desenha_linha('=', maior_texto + 4)
     for texto in titulos:
-        centraliza = (maior_texto - len(texto)) // 2
-        print(f'|{" " * centraliza} {texto} '
-              f'{" " * centraliza if len(texto) % 2 == 0 else " " * (centraliza + 1)}|')
+        completa_espaco = maior_texto - len(texto)
+        print(f'| {texto}'
+              f'{" " * completa_espaco}|')
     desenha_linha('=', maior_texto + 4)
+
+
+def cria_menu(menu: list):
+    maior_texto = len(menu[0])
+    for item in menu:
+        tamanho_texto = len(item)
+        if tamanho_texto > maior_texto:
+            maior_texto = tamanho_texto
+    desenha_linha('=', maior_texto + 9)
+    for i, item in enumerate(menu):
+        completa_espaco = maior_texto - len(item)
+        if i < 10:
+            print(f'|  {i + 1} - {item}'
+                  f'{" " * completa_espaco} |')
+        else:
+            print(f'| {i + 1} - {item}'
+                  f'{" " * completa_espaco}|')
+    desenha_linha('=', maior_texto + 9)
 
 
 if __name__ == '__main__':
